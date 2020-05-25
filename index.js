65c5e346ae18b9732e77bee5ba11f5187d134809
lojoyu <sophie200429@gmail.com>
2020-05-25 17:22:16 +0800

    origin server

--- index.js
+++ index.js
@@ -3,24 +3,18 @@ const socketio = require('socket.io');
 const port = process.env.PORT || 8000
 
 const server = http.createServer((req, res) => {
-    res.end("I am connected!")
+    res.end('connected!')
 });
 
 const io = socketio(server);
 
 io.on('connection', (socket, req) => {
 
-    socket.emit('welcome', {
-        'dd': 'yoyoyo'
-    })
-    
-
     socket.on('osc', (data) => {
         console.log(data);
         io.emit('osc', data);
         //io.emit(data.address, data.args[0].value);
     })
-
 
     socket.on('connected', (data) => {
         console.log("connected");
@@ -31,9 +25,13 @@ io.on('connection', (socket, req) => {
         
     })
 
+    socket.on("*",function(event,data) {
+        console.log(event);
+        console.log(data);
+    });
+
 })
 
 server.listen(port, function listening() {
     console.log("Listening on %d", server.address().port);
 });
-
